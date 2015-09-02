916b00905d1dc90390be17784b15bbb0b9d3f7a7
Hao Yu (Cody) <comaniac0422@gmail.com>
2015-09-01 18:03:18 -0700

    Prepare sync with master

--- examples/logisticRegression/app/run_local.sh
+++ examples/logisticRegression/app/run_local.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 spark-submit --class LogisticRegression \
-	--jars ${BLAZE_HOME}/aparapi_blaze/com.amd.aparapi/dist/aparapi.jar,${BLAZE_HOME}/accrdd/target/BLAZE_RUNTIME-1.0-SNAPSHOT.jar \
+	--jars ${BLAZE_HOME}/target/BLAZE_RUNTIME-1.0-SNAPSHOT.jar \
 	--master local[*] \
 	target/logisticRegression-0.0.0.jar ../train_data.txt 10 3
 
 #	--driver-memory 1G \
 #	--executor-memory 1G \
-#	--driver-java-options " -Djava.library.path=${BLAZE_HOME}../aparapi_blaze/com.amd.aparapi.jni/dist" \
 
